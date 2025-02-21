0ae9e8bc5afaa3f109e9d6e38a0fb0ce34062229
tingirifistik <eismet.yildiz@gmail.com>
2025-02-21 18:38:25 +0100

    update

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ python3 indir.py
 ```
 
 
+
+<h2>Access Token Nerede?</h2>
+
+BTK Akademi'ye giriş yaptıktan sonra tarayıcı konsolunun _Storage_ sekmesindeki _Cookies_ kısmında _access_token_ değerini bulabilirsiniz.
--- indir.py
+++ indir.py
@@ -1,24 +1,26 @@
-import requests
+from requests import get, post, session
 from os import system
 from time import sleep
+from yt_dlp import YoutubeDL
 
-AUTHORIZATION = ""
+ACCESS_TOKEN = ""
 
 url = input("Url: ")
 kurs = url.split("-")[-1]
 
 system("cls||clear")
+
 url = f"https://www.btkakademi.gov.tr:443/api/service/v1/course/registration/register/{kurs}?language=tr"
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0", "Accept": "application/json, text/plain, */*", "Accept-Language": "tr-TR,tr;q=0.8,en-US;q=0.5,en;q=0.3", "Accept-Encoding": "gzip, deflate, br", "Content-Type": "application/json", "Authorization": "Bearer "+AUTHORIZATION, "Origin": "https://www.btkakademi.gov.tr", "Dnt": "1", "Referer": "https://www.btkakademi.gov.tr/portal/course/microsoft-powerpoint-13365", "Sec-Fetch-Dest": "empty", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Site": "same-origin", "Te": "trailers", "Connection": "close"}
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0", "Accept": "application/json, text/plain, */*", "Accept-Language": "tr-TR,tr;q=0.8,en-US;q=0.5,en;q=0.3", "Accept-Encoding": "gzip, deflate, br", "Content-Type": "application/json", "Authorization": "Bearer "+ACCESS_TOKEN, "Origin": "https://www.btkakademi.gov.tr", "Dnt": "1", "Referer": "", "Sec-Fetch-Dest": "empty", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Site": "same-origin", "Te": "trailers", "Connection": "close"}
 json={"demandForm": {}}
-print(requests.post(url, headers=headers, json=json).json()["status"])
+print(post(url, headers=headers, json=json).json()["status"])
+
 sleep(1)
 system("cls||clear")
 
 url = f"https://www.btkakademi.gov.tr:443/api/service/v1/public/51/course/details/program/syllabus/{kurs}?language=tr"
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0", "Accept": "application/json, text/plain, */*", "Accept-Language": "tr-TR,tr;q=0.8,en-US;q=0.5,en;q=0.3", "Accept-Encoding": "gzip, deflate, br", "Authorization": "Bearer "+AUTHORIZATION, "Dnt": "1", "Referer": "https://www.btkakademi.gov.tr/portal/course/uygulamali-microsoft-power-bi-13323", "Sec-Fetch-Dest": "empty", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Site": "same-origin", "Te": "trailers", "Connection": "close"}
-r = requests.get(url, headers=headers).json()
-
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0", "Accept": "application/json, text/plain, */*", "Accept-Language": "tr-TR,tr;q=0.8,en-US;q=0.5,en;q=0.3", "Accept-Encoding": "gzip, deflate, br", "Authorization": "Bearer "+ACCESS_TOKEN, "Dnt": "1", "Referer": "", "Sec-Fetch-Dest": "empty", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Site": "same-origin", "Te": "trailers", "Connection": "close"}
+r = get(url, headers=headers).json()
 
 x = 0
 while 1: 
@@ -39,24 +41,27 @@ while 1:
         break
     x+=1
 sec2 = int(input("\n\nSeçim: "))
-
 id = str(r[sec-1]["courses"][sec2-1]["id"])
 
+system("cls||clear")
+
 url = "https://www.btkakademi.gov.tr:443/api/service/v1/course/deliver/start/"+id
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0", "Accept": "*/*", "Accept-Language": "tr-TR,tr;q=0.8,en-US;q=0.5,en;q=0.3", "Accept-Encoding": "gzip, deflate, br", "Referer": "https://www.btkakademi.gov.tr/portal/course/player/deliver/micropython-ile-microbit-programlama-19668", "Content-Type": "application/json", "Authorization": "Bearer "+AUTHORIZATION, "Origin": "https://www.btkakademi.gov.tr", "Dnt": "1", "Sec-Fetch-Dest": "empty", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Site": "same-origin", "Te": "trailers", "Connection": "close"}
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0", "Accept": "*/*", "Accept-Language": "tr-TR,tr;q=0.8,en-US;q=0.5,en;q=0.3", "Accept-Encoding": "gzip, deflate, br", "Referer": "", "Content-Type": "application/json", "Authorization": "Bearer "+ACCESS_TOKEN, "Origin": "https://www.btkakademi.gov.tr", "Dnt": "1", "Sec-Fetch-Dest": "empty", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Site": "same-origin", "Te": "trailers", "Connection": "close"}
 json={"programId": int(kurs)}
-video_id = requests.post(url, headers=headers, json=json).json()["remoteCourseReference"]
+try:
+    video_id = post(url, headers=headers, json=json).json()["remoteCourseReference"]
+except:
+    print("ACCESS_TOKEN eksik!!")
+    sleep(2)
+    exit()
 
-
-session = requests.session()
+oturum = session()
 url = f"https://cinema8.com:443/raw-video/{video_id}?c=js-api&sub=tr&t=s0"
 cookies = {"JSESSIONID": "31E6BD18E17DEA8137079E9954B99008.jvm4"}
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8", "Accept-Language": "tr-TR,tr;q=0.8,en-US;q=0.5,en;q=0.3", "Accept-Encoding": "gzip, deflate, br", "Dnt": "1", "Referer": "https://www.btkakademi.gov.tr/", "Upgrade-Insecure-Requests": "1", "Sec-Fetch-Dest": "iframe", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-Site": "cross-site", "Te": "trailers", "Connection": "close"}
-session.get(url, headers=headers, cookies=cookies)
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8", "Accept-Language": "tr-TR,tr;q=0.8,en-US;q=0.5,en;q=0.3", "Accept-Encoding": "gzip, deflate, br", "Dnt": "1", "Referer": "", "Upgrade-Insecure-Requests": "1", "Sec-Fetch-Dest": "iframe", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-Site": "cross-site", "Te": "trailers", "Connection": "close"}
+oturum.get(url, headers=headers, cookies=cookies)
 
-system("cls||clear")
-print(video_id)
-json_file = session.get(f"https://cinema8.com/api/v1/uscene/rawvideo/flavor/{video_id}").json()
-with open(json_file["name"].split(".mp4")[0]+".m3u8", "wb") as f:
-    print("{} --> Indiriliyor...".format(json_file["name"].split(".mp4")[0]))
-    f.write(session.get(json_file["hlsUrl"]).content)
+json_file = oturum.get(f"https://cinema8.com/api/v1/uscene/rawvideo/flavor/{video_id}").json()
+with YoutubeDL({'outtmpl': json_file["name"].split(".mp4")[0]+'.mp4'}) as ydl:
+    print("{} --> Indiriliyor...".format(json_file["name"].split(".mp4")[0])+"\n\n")
+    ydl.download(json_file["hlsUrl"])
